03c94c8d4965d13d130213d4172b671436205b53
+ <+>
2023-09-06 15:22:13 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+
+
+function writeCards(names, eventName) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`); 
+    }
+    return newArray
+}
+
+
+function countDown(positiveInteger) {
+    let i = positiveInteger
+    while (i >= 0) {
+        console.log(i--)
+    }
+    return positiveInteger
+}
+
+countDown(69)
